e32bc2e9454ffcf9d9e0f892840e7674068fc5ec
Svetlana Staneva <smstaneva@gmail.com>
2017-01-11 18:29:52 +0000

    Swirl - Matrices and Data Frames

---
+++ drill/swirl_matrices_and_data_frames.R
@@ -0,0 +1,31 @@
+my_vector <- 1:20
+my_vector
+
+dim(my_vector)
+
+length(my_vector)
+
+dim(my_vector) <- c(4, 5)
+dim(my_vector)
+
+attributes(my_vector)
+
+my_vector
+
+class(my_vector)
+
+my_matrix <- my_vector
+my_matrix2 <- matrix(1:20, 4, 5)
+identical(my_matrix, my_matrix2)
+
+patients <- c("Bill", "Gina", "Kelly", "Sean")
+cbind(patients, my_matrix)
+
+my_data <- data.frame(patients, my_matrix)
+my_data
+
+class(my_data)
+
+cnames <- c("patient", "age", "weight", "bp", "rating", "test")
+colnames(my_data) <- cnames
+my_data
