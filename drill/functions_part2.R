6af1611ddb68f9cfc3c292fefcc89672a741f539
Svetlana Staneva <smstaneva@gmail.com>
2017-01-15 13:05:30 +0000

    Functions (Part 2)

---
+++ drill/functions_part2.R
@@ -0,0 +1,20 @@
+f <- function(a, b){
+  a^2
+}
+f(2)
+
+f <- function(a, b){
+  print(a)
+  print(b)
+}
+f(45)
+
+mean
+
+args(paste)
+function(..., sep = " ", collapse = NULL)
+
+paste("a", "b", sep = ":")
+
+paste("a", "b", se = ":")
+
