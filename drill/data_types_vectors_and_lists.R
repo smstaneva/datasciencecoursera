7d6900d88ae0e288174de548eb8055ba46a4693d
Svetlana Staneva <smstaneva@gmail.com>
2017-01-19 11:35:35 +0000

    Data Types - Vectors and Lists

---
+++ drill/data_types_vectors_and_lists.R
@@ -0,0 +1,21 @@
+x <- vector("numeric", length = 10)
+x
+
+x <- 0:6
+class(x)
+
+as.numeric(x)
+
+as.logical(x)
+
+as.character(x)
+
+x <- c("a", "b", "c")
+as.numeric(x)
+
+as.logical(x)
+
+as.complex(x)
+
+x <- list(1, "a", TRUE, 1 + 4i)
+x
