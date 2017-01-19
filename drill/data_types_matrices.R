90cc5762201776a55977d0518262533e83e221d6
Svetlana Staneva <smstaneva@gmail.com>
2017-01-19 12:09:10 +0000

    Data Types - Matrices

---
+++ drill/data_types_matrices.R
@@ -0,0 +1,21 @@
+m <- matrix(nrow = 2, ncol = 3)
+m
+
+dim(m)
+
+attributes(m)
+
+m <- matrix(1:6, nrow = 2, ncol = 3)
+m
+
+m <- 1:10
+m
+
+dim(m) <- c(2, 5)
+m
+
+x <- 1:3
+y <- 10:12
+cbind(x, y)
+
+rbind(x, y)
