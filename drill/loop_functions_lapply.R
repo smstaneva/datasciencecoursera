2563a519d3f4d07c83e77f3262105ea57cf12d98
Svetlana Staneva <smstaneva@gmail.com>
2017-01-23 11:24:26 +0000

    Loop Functions - lapply

---
+++ drill/loop_functions_lapply.R
@@ -0,0 +1,24 @@
+x <- list(a = 1:5, b = rnorm(10))
+lapply(x, mean)
+
+x <- list(a = 1:4, b = rnorm(10), c = rnorm(20, 1), d = rnorm(100, 5))
+lapply(x, mean)
+
+x <- 1:4
+lapply(x, runif)
+
+x <- 1:4
+lapply(x, runif, min = 0, max = 10)
+
+x <- list(a = matrix(1:4, 2, 2), b = matrix(1:6, 3, 2))
+x
+
+lapply(x, function(elt) elt[,1])
+
+x <- list(a = 1:4, b = rnorm(10), c = rnorm(20, 1), d = rnorm(100, 5))
+lapply(x, mean)
+
+sapply(x, mean)
+
+mean(x)
+
