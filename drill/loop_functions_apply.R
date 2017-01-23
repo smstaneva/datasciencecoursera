f2f23732917bff83d77b228a96766c12a7d8c463
Svetlana Staneva <smstaneva@gmail.com>
2017-01-23 11:26:06 +0000

    Loop Functions - apply

---
+++ drill/loop_functions_apply.R
@@ -0,0 +1,14 @@
+str(apply)
+
+x <- matrix(rnorm(200), 20, 10)
+apply(x, 2, mean)
+
+apply(x, 1, sum)
+
+x <- matrix(rnorm(200), 20, 10)
+apply(x, 1, quantile, probs = c(0.25, 0.75))
+
+a <- array(rnorm(2 * 2 * 10), c(2, 2, 10))
+apply(a, c(1, 2), mean)
+
+rowMeans(a, dims = 2)
