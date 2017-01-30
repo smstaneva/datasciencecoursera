de189a345ee1972bb9a536bffcd28ecc88f37838
Svetlana Staneva <smstaneva@gmail.com>
2017-01-30 14:12:18 +0000

    Ggplot2 - Introduction to R and RStudio

---
+++ drill/ggplot2_introduction_to_r_and_rstudio.R
@@ -0,0 +1,72 @@
+1 + 100
+
+1 +
+
+3 + 5 * 2
+
+(3 + 5) * 2
+
+(3 + (5 * (2 ^ 2)))
+
+3 + 5 * 2 ^ 2
+
+3 + 5 * (2 ^ 2)
+
+2/10000
+
+5e3
+
+sin(1)
+
+log(1)
+
+log10(10)
+
+exp(0.5)
+
+1 == 1
+
+1 != 2
+
+1 <  2
+
+1 <= 1
+
+1 > 0
+
+1 >= -9
+
+x <- 1/40
+x
+
+log(x)
+
+1:5
+
+2^(1:5)
+
+x <- 1:5
+2^x
+
+ls()
+
+ls
+
+mass <- 47.5
+mass
+
+age <- 122
+age
+
+mass <- mass * 2.3
+mass
+
+age <- age - 20
+age
+
+mass > age
+
+install.packages("plyr")
+
+install.packages("gapminder")
+
