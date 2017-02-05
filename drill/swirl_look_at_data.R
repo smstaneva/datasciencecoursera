504ca34cd831f4d4c2d867d8145d0822e2573be4
Svetlana Staneva <smstaneva@gmail.com>
2017-02-05 16:27:45 +0000

    Swirl - Look at Data

---
+++ drill/swirl_look_at_data.R
@@ -0,0 +1,25 @@
+ls()
+
+class(plants)
+
+dim(plants)
+
+nrow(plants)
+
+ncol(plants)
+
+object.size(plants)
+
+names(plants)
+
+head(plants)
+
+head(plants, 10)
+
+tail(plants, 15)
+
+summary(plants)
+
+table(plants$Active_Growth_Period)
+
+str(plants)
