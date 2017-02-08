4dc9f812c7d290ff40adf67c170193d9c6504e7f
Svetlana Staneva <smstaneva@gmail.com>
2017-02-08 19:28:24 +0000

    Add rankhospital.R - final

--- rankhospital.R
+++ rankhospital.R
@@ -1,19 +1,34 @@
-rankhospital <- function(state, outcome, num = "best"){
+rankhospital <- function(state, outcome, num = "best") {
+        
         care <- read.csv(file = "ProgrammingAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
         
-        if (!state %in% unique(care[, 7])) {
-                stop("invalid state")
-        }
-        switch(outcome, "heart attack" = {
-                col = 11
-        }, 
-        "heart failure" = {
-                col = 19
-        },
-        pneumonia = {
-                col = 23
-        }, stop("invalid outcome"))
+                if (!state %in% unique(care[, 7])) {
+                        stop("invalid state")
+                }
+                switch(outcome, "heart attack" = {
+                        col = 11
+                }, "heart failure" = {
+                        col = 17
+                }, pneumonia = {
+                        col = 23
+                }, stop("invalid outcome"))
         
         
+        care[, col] <- as.numeric(care[, col])
+        df <- care[care$State == state, c(2, col)]
+        df <- na.omit(df)
         
+                switch(num, best = {
+                        num = 1
+                }, worst = {
+                        num = nrow(df)
+                })
+                if (num > nrow(df)) {
+                        return(NA)
+                }
+        
+        rank <- order(df[, 2], df[, 1])
+        df[rank, ][num, 1]
 }
+
+
