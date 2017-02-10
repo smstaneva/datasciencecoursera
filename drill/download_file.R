9f996a2f0486ddfddbd65c1c4154031ae1101cbb
Svetlana Staneva <smstaneva@gmail.com>
2017-02-10 13:50:15 +0000

    Download File

---
+++ drill/download_file.R
@@ -0,0 +1,13 @@
+if(!file.exists("data")){
+        dir.create("data")
+}
+
+
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl, destfile = "./data/camera.csv", method = "curl")
+list.files("./data")
+
+
+dateDownloaded <- date()
+dateDownloaded
+
