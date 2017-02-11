01ecbdc82c388b14e38168ca134f85a7d5c7bb52
Svetlana Staneva <smstaneva@gmail.com>
2017-02-11 15:30:02 +0000

    Read Local File

---
+++ drill/read_local_file.R
@@ -0,0 +1,9 @@
+cameraData <- read.table("./data/camera.csv")
+
+head(cameraData)
+
+cameraData <- read.table("./data/camera.csv", sep = ",", header = TRUE)
+head(cameraData)
+
+cameraData <- read.csv("./data/camera.csv")
+head(cameraData)
