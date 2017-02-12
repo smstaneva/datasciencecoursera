efc50d57d5f4ad376eb68cf3b4c47724f3a13c05
Svetlana Staneva <smstaneva@gmail.com>
2017-02-12 14:32:32 +0000

    Read XML

---
+++ drill/read_xml.R
@@ -0,0 +1,26 @@
+install.packages("XML")
+library(XML)
+fileUrl <- "http://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse(fileUrl, useInternal = TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+
+names(rootNode)
+
+rootNode[[1]]
+
+xpathSApply(rootNode, "//name", xmlValue)
+
+xpathSApply(rootNode, "//price", xmlValue)
+
+rootNode[[1]][[1]]
+
+xmlSApply(rootNode, xmlValue)
+
+fileUrl <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+doc <- htmlTreeParse(fileUrl, useInternal = TRUE)
+scores <- xpathSApply(doc, "//li[@class = 'score']", xmlValue)
+teams <- xpathSApply(doc, "//li[@class = 'team-name']", xmlValue)
+scores
+
+teams
