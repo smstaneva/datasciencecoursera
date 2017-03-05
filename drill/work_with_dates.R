4dd4b666a0df895899cf9ae048e94c7b1a2d38ab
Svetlana Staneva <smstaneva@gmail.com>
2017-03-05 16:54:30 +0000

    Work with Dates

---
+++ drill/work_with_dates.R
@@ -0,0 +1,39 @@
+d1 = date()
+d1
+
+class(d1)
+
+d2 = Sys.Date()
+d2
+
+class(d2)
+
+format(d2, "%a %b %d")
+
+x = c("1jan1960", "2jan1960", "31mar1960", "30jul1960"); z = as.Date(x, "%d%b%Y")
+z
+
+z[1] - z[2]
+
+as.numeric(z[1]-z[2])
+
+weekdays(d2)
+
+months(d2)
+
+julian(d2)
+
+library(lubridate); ymd("20140108")
+
+mdy("08/04/2013")
+
+dmy("03-04-2013")
+
+ymd_hms("2011-08-03 10:15:03")
+
+ymd_hms("2011-08-03 10:15:03", tz = "Pacific/Auckland")
+
+x = dmy(c("1jan2013", "2jan2013", "31mar2013", "30jul2013"))
+wday(x[1])
+
+wday(x[1], label = TRUE)
