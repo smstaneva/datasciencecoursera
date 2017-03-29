4fe72b8b689df975e0a67bcdc5885949dac47a39
Svetlana Staneva <smstaneva@gmail.com>
2017-03-29 13:58:42 +0100

    Read the data from the dates

--- ProgrammingAssignment13-EDA/plot1.R
+++ ProgrammingAssignment13-EDA/plot1.R
@@ -7,6 +7,11 @@
 # required = 0.149418648GB * 2 = 0.298837296GB
 # required < RAM
 
+# read the data from the dates 2007-02-01 and 2007-02-02
+
+# head -n 3 household_power_consumption.txt
+# grep "Date\|1/2/2007\|2/2/2007" household_power_consumption.txt > grep.txt
+# head -n 3 grep.txt
 
 
 
