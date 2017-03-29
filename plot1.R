5efc372079eed05d2859331df69c6e18fbc7cc20
Svetlana Staneva <smstaneva@gmail.com>
2017-03-29 13:55:45 +0100

    Calculate estimate of required memory

---
+++ plot1.R
@@ -0,0 +1,15 @@
+# calculate a rough estimate of how much memory the dataset will require
+
+# nrow = 2,075,259
+# ncol = 9
+# GB = ?
+# 2,075,259 * 9 * 8 bytes/numeric = 149418648 bytes = 0.149418648GB
+# required = 0.149418648GB * 2 = 0.298837296GB
+# required < RAM
+
+
+
+
+
+
+
