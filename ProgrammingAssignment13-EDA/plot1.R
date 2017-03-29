a3486636a57dd9495328dc7f5d3101c85cab2e30
Svetlana Staneva <smstaneva@gmail.com>
2017-03-29 14:00:35 +0100

    Read the data from the dates

--- ProgrammingAssignment13-EDA/plot1.R
+++ ProgrammingAssignment13-EDA/plot1.R
@@ -13,6 +13,9 @@
 # grep "Date\|1/2/2007\|2/2/2007" household_power_consumption.txt > grep.txt
 # head -n 3 grep.txt
 
+system.time(consumption <- read.table("./data/grep.txt", sep = ";", header = TRUE)) 
+head(consumption)
+
 
 
 
