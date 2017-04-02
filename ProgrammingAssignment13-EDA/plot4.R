8e4665bfa23cc3c14a4d139bd0504ed183784d65
Svetlana Staneva <smstaneva@gmail.com>
2017-04-02 09:05:01 +0100

    Set margins

--- ProgrammingAssignment13-EDA/plot4.R
+++ ProgrammingAssignment13-EDA/plot4.R
@@ -4,7 +4,7 @@ consumption <- read.table("./data/grep-v.txt", sep = ";", header = TRUE, strings
 
 Time <- strptime(consumption$Time, "%H:%M:%S")
 
-par(mfrow = c(2, 2))
+par(mfrow = c(2, 2), mar = c(4, 4, 2, 1))
 
 plot(consumption$Global_active_power,
      type = "l",
@@ -26,7 +26,8 @@ plot(consumption$Sub_metering_1,
      xlab = "",
      ylab = "Energy sub metering",
      type = "l",
-     col = "black")
+     col = "black", 
+     bty = "o")
 axis(2, ylim = c(0, max(consumption$Sub_metering_1)), col = "black", lwd = 2)
 
 
@@ -50,6 +51,7 @@ plot(consumption$Sub_metering_3,
 
 legend("topright", pch = 150, bty = "n", col = c("black", "#cd0033", "#4bc6df"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 axis(1, at = c(0, 1500, 2900), labels = c("Thu", "Fri", "Sat"))
+box()
 
 plot(consumption$Global_reactive_power,
      type = "l",
@@ -57,3 +59,4 @@ plot(consumption$Global_reactive_power,
      ylab = "Global_reactive_power", 
      xaxt = "n")
 axis(1, at = c(0, 1500, 2900), labels = c("Thu", "Fri", "Sat"))
+box()
