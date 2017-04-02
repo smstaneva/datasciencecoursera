d21a6aa158d64ed69cc0bc7e1c2f8e0f24774fc9
Svetlana Staneva <smstaneva@gmail.com>
2017-04-02 08:45:01 +0100

    Reconstruct Plot 4

---
+++ ProgrammingAssignment13-EDA/plot4.R
@@ -0,0 +1,59 @@
+# reconstruct Plot 4 using the base plotting system
+
+consumption <- read.table("./data/grep-v.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
+
+Time <- strptime(consumption$Time, "%H:%M:%S")
+
+par(mfrow = c(2, 2))
+
+plot(consumption$Global_active_power,
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power", 
+     xaxt = "n")
+axis(1, at = c(0, 1500, 2900), labels = c("Thu", "Fri", "Sat"))
+
+plot(consumption$Voltage,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Voltage", 
+     xaxt = "n")
+axis(1, at = c(0, 1500, 2900), labels = c("Thu", "Fri", "Sat"))
+
+plot(consumption$Sub_metering_1,
+     axes = FALSE,
+     ylim = range(c(consumption$Sub_metering_1, consumption$Sub_metering_2, consumption$Sub_metering_3)),
+     xlab = "",
+     ylab = "Energy sub metering",
+     type = "l",
+     col = "black")
+axis(2, ylim = c(0, max(consumption$Sub_metering_1)), col = "black", lwd = 2)
+
+
+par(new = T)
+plot(consumption$Sub_metering_2,
+     axes = FALSE,
+     ylim = range(c(consumption$Sub_metering_1, consumption$Sub_metering_2, consumption$Sub_metering_3)),
+     xlab = "",
+     ylab = "", 
+     type = "l",
+     col = "#cd0033")
+
+par(new = T)
+plot(consumption$Sub_metering_3,
+     axes = FALSE,
+     ylim = range(c(consumption$Sub_metering_1, consumption$Sub_metering_2, consumption$Sub_metering_3)),
+     xlab = "",
+     ylab = "", 
+     type = "l",
+     col = "#4bc6df")
+
+legend("topright", pch = 150, bty = "n", col = c("black", "#cd0033", "#4bc6df"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+axis(1, at = c(0, 1500, 2900), labels = c("Thu", "Fri", "Sat"))
+
+plot(consumption$Global_reactive_power,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Global_reactive_power", 
+     xaxt = "n")
+axis(1, at = c(0, 1500, 2900), labels = c("Thu", "Fri", "Sat"))
