01d6bcdc8e0c3001dc75128f20bbe6643e7767eb
Svetlana Staneva <smstaneva@gmail.com>
2017-04-14 13:33:03 +0100

    K-Means Clusterig (Part 2)

---
+++ drill/k_means_clustering_part2.R
@@ -0,0 +1,18 @@
+dataFrame <- data.frame(x, y)
+kmeansObj <- kmeans(dataFrame, centers = 3)
+names(kmeansObj)
+
+kmeansObj$cluster
+
+par(mar = rep(0.2, 4))
+plot(x, y, col = kmeansObj$cluster, pch = 19, cex = 2)
+
+points(kmeansObj$centers, col = 1:3, pch = 3, cex = 3, lwd = 3)
+
+set.seed(1234)
+dataMatrix <- as.matrix(dataFrame)[sample(1:12), ]
+kmeansObj2 <- kmeans(dataMatrix, centers = 3)
+par(mfrow = c(1, 2), mar = c(2, 4, 0.1, 0.1))
+image(t(dataMatrix)[, nrow(dataMatrix): 1], yaxt = "n")
+
+image(t(dataMatrix)[, order(kmeansObj$cluster)], yaxt = "n")
