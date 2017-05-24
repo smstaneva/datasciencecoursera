14551029a385e3c128f3b1b01076ea22125c4249
Svetlana Staneva <smstaneva@gmail.com>
2017-05-24 21:52:45 +0100

    Check for Missing Values

--- ProgrammingAssignment14-EDA/plot1.R
+++ ProgrammingAssignment14-EDA/plot1.R
@@ -4,3 +4,15 @@ dir()
 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+dim(NEI)
+
+head(NEI)
+
+# check for missing values
+
+x0 <- NEI$Emissions
+str(x0)
+
+mean(is.na(x0))
+
