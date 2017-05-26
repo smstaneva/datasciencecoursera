ad54e769e50f70cb45e4fca616d94985a8ec8a8d
Svetlana Staneva <smstaneva@gmail.com>
2017-05-26 22:14:33 +0100

    Construct Plot 1 Using the Base Plotting System

--- ProgrammingAssignment14-EDA/plot1.R
+++ ProgrammingAssignment14-EDA/plot1.R
@@ -20,3 +20,24 @@ mean(is.na(x0))
 
 class(x0)
 
+# construct plot1 using the base plotting system
+
+pm0 <- NEI[which(NEI$year == 1999),"Emissions"]
+summary(pm0)
+
+pm1 <- NEI[which(NEI$year == 2002), "Emissions"]
+summary(pm1)
+
+pm2 <- NEI[which(NEI$year == 2005), "Emissions"]
+summary(pm2)
+
+pm3 <- NEI[which(NEI$year == 2008), "Emissions"]
+summary(pm3)
+
+lmts <- range(pm0, pm1, pm2, pm3)
+
+boxplot(pm0, pm1, pm2, pm3,
+        ylim = lmts,
+        ylab = "Total 2.5 Emissions", 
+        names = c("1999", "2002", "2005", "2008"))
+axis(2, ylim = c(0, max(pm1["Emissions"])))
