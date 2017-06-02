bfe80d61c0cff335d9db6b18ef294a9161dc9509
Svetlana Staneva <smstaneva@gmail.com>
2017-06-02 21:35:07 +0100

    Construct Plot Using Base Plotting System

--- ProgrammingAssignment14-EDA/plot2.R
+++ ProgrammingAssignment14-EDA/plot2.R
@@ -2,3 +2,9 @@
 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+# construct plot2 using the base plotting system
+
+NEI <- cbind(rownames(NEI), NEI)
+rownames(NEI) <- NULL
+colnames(NEI) <- c("State.Code", "fips", "SCC", "Pollutant", "Emissions", "type", "year")
