6009335ca077965c38bc01a0517f616aae3f58ab
Svetlana Staneva <smstaneva@gmail.com>
2017-07-22 17:24:43 +0100

    Check class

--- ProgrammingAssignment14-EDA/plot1.R
+++ ProgrammingAssignment14-EDA/plot1.R
@@ -5,3 +5,47 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 dim(NEI)
 head(NEI)
+
+# check class
+
+x0 <- NEI$Emissions
+class(x0)
+
+str(x0)
+summary(x0)
+
+pm1<- NEI[which(NEI$year == 1999), ]
+dim(pm1)
+head(pm1)
+str(pm1)
+summary(pm1)
+
+pm2<- NEI[which(NEI$year == 2002), ]
+dim(pm2)
+head(pm2)
+str(pm2)
+summary(pm2)
+
+pm3<- NEI[which(NEI$year == 2005), ]
+dim(pm3)
+head(pm3)
+str(pm3)
+summary(pm3)
+
+pm4<- NEI[which(NEI$year == 2008), ]
+dim(pm4)
+head(pm4)
+str(pm4)
+summary(pm4)
+
+x1 <- pm1$Emissions
+summary(x1)
+
+x2<- pm2$Emissions
+summary(x2)
+
+x3<- pm3$Emissions
+summary(x3)
+
+x4<- pm4$Emissions
+summary(x4)
