64bce017c9f33bc06f438ce5200c4f8d3e6bc79f
Svetlana Staneva <smstaneva@gmail.com>
2017-08-20 21:20:52 +0100

    Construct Plot 3 Using ggplot2 Plotting System

--- ProgrammingAssignment14-EDA/plot3.R
+++ ProgrammingAssignment14-EDA/plot3.R
@@ -6,3 +6,11 @@ SCC <- readRDS("Source_Classification_Code.rds")
 pm1 <- subset(NEI, fips == "24510")
 
 head(pm1)
+
+# construct plot3 using ggplot2 plotting system
+
+library("ggplot2")
+
+ggplot(data = pm1, aes(x = year, y = Emissions, color = type)) +
+        geom_line() + 
+        facet_wrap( ~ type)
