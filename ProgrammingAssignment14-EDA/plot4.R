2b58cc87b768feca31871e81fe410da249d88197
Svetlana Staneva <smstaneva@gmail.com>
2017-08-20 21:50:42 +0100

    Construct Plot 4 Using Base Plotting System

--- ProgrammingAssignment14-EDA/plot4.R
+++ ProgrammingAssignment14-EDA/plot4.R
@@ -16,3 +16,10 @@ X1 <- PM1$Emissions
 X2 <- PM2$Emissions
 X3 <- PM3$Emissions
 X4 <- PM4$Emissions
+
+# construct plot4 using base plotting system
+boxplot(X1, X2, X3, X4)
+
+boxplot(log10(X1), log10(X2), log10(X3), log10(X4), 
+        main = "Emissions from Coal Combustion - Related Sources across USA",
+        col = "#bfb6b4")
