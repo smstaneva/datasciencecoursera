da27e9991ce748626622a9365b6d1588e2032eb9
Svetlana Staneva <smstaneva@gmail.com>
2017-08-21 21:15:56 +0100

    Construct Plot 5 Using Base Plotting System

--- ProgrammingAssignment14-EDA/plot5.R
+++ ProgrammingAssignment14-EDA/plot5.R
@@ -16,3 +16,11 @@ X1 <- PM1$Emissions
 X2 <- PM2$Emissions
 X3 <- PM3$Emissions
 X4 <- PM4$Emissions
+
+# construct plot5 using base plotting system
+
+boxplot(X1, X2, X3, X4)
+
+boxplot(log10(X1), log10(X2), log10(X3), log10(X4), 
+        main = "Emissions from Motor Vehicle Sources in Baltimore City",
+        col = "#020a40")
