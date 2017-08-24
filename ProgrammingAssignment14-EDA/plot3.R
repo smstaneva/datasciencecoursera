d05488867cba8f48a287b5e64a5876cbe072b1da
Svetlana Staneva <smstaneva@gmail.com>
2017-08-24 20:59:12 +0100

    Save Plot 3 to a PNG File

--- ProgrammingAssignment14-EDA/plot3.R
+++ ProgrammingAssignment14-EDA/plot3.R
@@ -11,8 +11,10 @@ head(pm1)
 
 library("ggplot2")
 
-ggplot(data = pm1, aes(x = year, y = Emissions, color = type)) +
-        geom_line() + 
+ggplot(pm1, aes(x = year, y = Emissions, color = type)) +
+        geom_line(size = 1) +
+        scale_y_log10() +
+        scale_color_manual(values=c("#800020", "#4BC6DF", "#BFB6B4", "#020A40")) +
         facet_wrap( ~ type) +
         ggtitle("PM2.5 Emissions in Baltimore City by Type")
 
