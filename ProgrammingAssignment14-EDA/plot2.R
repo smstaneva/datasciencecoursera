f806536cc7a88bc9463d8a9f12e5f6bfa116434f
Svetlana Staneva <smstaneva@gmail.com>
2017-10-01 17:04:32 +0100

    Save Plot 2 to a PNG File

--- ProgrammingAssignment14-EDA/plot2.R
+++ ProgrammingAssignment14-EDA/plot2.R
@@ -3,26 +3,27 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-pm1 <- subset(NEI, fips == "24510")
+x0 <- subset(NEI, fips == "24510")
 
-PM1 <- subset(pm1, pm1$year == 1999)
-PM2 <- subset(pm1, pm1$year == 2002)
-PM3 <- subset(pm1, pm1$year == 2005)
-PM4 <- subset(pm1, pm1$year == 2008)
+pm1 <- subset(x0, x0$year == 1999)
+pm2 <- subset(x0, x0$year == 2002)
+pm3 <- subset(x0, x0$year == 2005)
+pm4 <- subset(x0, x0$year == 2008)
 
-head(PM1)
+head(pm1)
 
-X1 <- PM1$Emissions
-X2 <- PM2$Emissions
-X3 <- PM3$Emissions
-X4 <- PM4$Emissions
+x1 <- pm1$Emissions
+x2 <- pm2$Emissions
+x3 <- pm3$Emissions
+x4 <- pm4$Emissions
 
 # construct plot2 using the base plotting system
 
-boxplot(X1, X2, X3, X4)
+boxplot(x1, x2, x3, x4)
 
-boxplot(log10(X1), log10(X2), log10(X3), log10(X4), 
-        main = "Total PM2.5 Emissions in Baltimore City, Maryland",
+boxplot(log10(x1), log10(x2), log10(x3), log10(x4), 
+        main = "Total Emissions from PM2.5 in Baltimore City, Maryland",
+        names = c("1999", "2002", "2005", "2008"),
         col = "#4bc6df")
 
 # save plot to a png file
