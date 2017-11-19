87526d8deda7365381e5432a352cf75e946d0735
Svetlana Staneva <smstaneva@gmail.com>
2017-11-19 21:42:20 +0000

    Save Plot 3 to PNG File

--- ProgrammingAssignment14-EDA/plot3.R
+++ ProgrammingAssignment14-EDA/plot3.R
@@ -5,18 +5,17 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 x0 <- subset(NEI, fips == "24510")
 
-x0$year <- factor(x0$year, labels = c("1999", "2002", "2005", "2008"))
-
 # construct plot3 using ggplot2 plotting system
 
 library("ggplot2")
 
-ggplot(x0, aes(x = year, y = Emissions, color = type)) +
+ggplot(x0, aes(factor(year), y = Emissions, color = type)) +
         geom_boxplot(size = 1) +
-        scale_y_log10() +
+        scale_y_log10()+
         scale_color_manual(values=c("#800020", "#4BC6DF", "#BFB6B4", "#020A40")) +
         facet_wrap( ~ type) +
         ggtitle("Emissions from PM2.5 in Baltimore City, MD by Type")
+     
 
 # save plot to a png file
 
