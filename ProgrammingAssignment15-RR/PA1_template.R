ae712dca04f3deb13612cf1293765a59899353bb
Svetlana Staneva <smstaneva@gmail.com>
2018-01-22 22:54:40 +0000

    Construct Histogram of Steps per Day

---
+++ ProgrammingAssignment15-RR/PA1_template.R
@@ -0,0 +1,18 @@
+# read in the dataset and/or process the data
+
+activity <- read.csv("./data/activity.csv")
+head(activity)
+
+# convert the Date variable to Date class
+
+class(activity$date)
+Date <- as.Date(activity$date)
+class(Date)
+
+# construct a histogram of the total number of steps taken each day
+
+hist(activity$steps, 
+     col = "#800020", 
+     main = "Total Number of Steps per Day", 
+     xlab = "Steps per Day", 
+     ylab = "Frequency")
