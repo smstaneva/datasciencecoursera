9a86bbee5ee2dd0534af54f30330fe18559836ed
Svetlana Staneva <smstaneva@gmail.com>
2018-04-11 15:33:08 +0100

    ggplot - geom_histogram Remove Missing Values

--- drill/e_hist_ggplot/e_hist_ggplot.R
+++ drill/e_hist_ggplot/e_hist_ggplot.R
@@ -12,13 +12,17 @@ histo
 
 install.packages("ggplot")
 library(ggplot2)
-           
-ggplot(df, aes(steps)) + 
-        geom_histogram(breaks = pretty(range(df$steps), n = nclass.Sturges(df$steps),min.n = 1), 
-                       fill = "#800020", 
-                       color = "#000000") +
+
+ggplot(subset(df, !is.na(steps)), aes(steps)) + 
+        geom_histogram(fill = "#800020",
+                       color = "#000000", 
+                       breaks = seq(0, 100, by = 20)) +
         labs(title = "Histogram of Steps per Day", x = "Steps per Day", y = "Frequency") + 
         theme(plot.title = element_text(hjust = 0.5))
+
+
+
+
 
 
  
